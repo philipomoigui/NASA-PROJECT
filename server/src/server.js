f4a6497862d6b1c9eca63645fc5083f467121514
Philip Omoigui <philip.omoigui@innotelligent.com>
2021-06-24 17:21:06 +0100

    Added getPlanets api

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
