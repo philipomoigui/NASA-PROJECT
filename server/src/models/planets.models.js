eb9267a72f3939acdb1981d08322e403c2a09081
Philip Omoigui <philip.omoigui@innotelligent.com>
2021-06-24 19:07:52 +0100

    Enabled cors and Added NASA Kepler data

--- server/src/models/planets.models.js
+++ server/src/models/planets.models.js
@@ -1,3 +1,39 @@
-const planets = [];
+const fs = require('fs');
+const path = require('path');
 
-module.exports = planets;
+const parse = require('csv-parse');
+
+const habitablePlanet = [];
+
+function isPlanetHabitable(data) {
+    return data.koi_disposition === "CONFIRMED" &&  data.koi_insol > 0.36 && data.koi_insol < 1.11
+    && data.koi_prad < 1.6
+}
+
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'Kepler_data.csv'))
+        .pipe(parse({
+            comment: "#",
+            columns: true
+        }))
+        .on("data", (data) => {
+            if(isPlanetHabitable(data)){
+                habitablePlanet.push(data);
+            }
+        })
+        .on("error", (err) => {
+            console.log(err);
+            reject(err);
+        })
+        .on("end", () => {
+            console.log(`${habitablePlanet.length} habitable planets`);
+        })
+        resolve();
+    })
+}
+
+module.exports = {
+    loadPlanetsData,
+    planets: habitablePlanet
+};
--- server/src/routes/planets/planet.controller.js
+++ server/src/routes/planets/planet.controller.js
@@ -1,4 +1,4 @@
-const planets = require('../../models/planets.models');
+const {planets} = require('../../models/planets.models');
 
 
 function getAllPlanets(req, res) {
