e9cd0e5030339ab899aa7ef4c24ee27c54cae995
Philip Omoigui <philip.omoigui@innotelligent.com>
2021-06-24 16:05:55 +0100

    first commit

---
+++ server/src/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const app = require('./app');
+
+const PORT = process.env.PORT || 6000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+});
