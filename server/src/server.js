75203da6631805d00e7bdb4f7152146ece3165c0
philip omoigui <philipomoigui@gmail.com>
2021-07-05 15:37:14 +0100

    Added mongoose schemas for mongodb  collections

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,30 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 const {loadPlanetsData} = require('./models/planets.models');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://nasa-api:fPlHc1yRT8qf0jH9@nasa.gg0za.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('Mongodb Connection Ready');
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+})
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
     await loadPlanetsData();
 
     server.listen(PORT, () => {
@@ -16,7 +33,3 @@ async function startServer() {
 }
 
 startServer();
-
-// fPlHc1yRT8qf0jH9
-
-// mongodb+srv://nasa-api:fPlHc1yRT8qf0jH9@nasa.gg0za.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
